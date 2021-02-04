25bd0b594140054cb34ac28b6875d7f468a7a2ca
Skaven <Skaven@Zhixings-MacBook-Pro.local>
2021-02-03 20:32:37 -0600

    new

---
+++ src/main/java/com/laioffer/job/entity/Item.java
@@ -0,0 +1,73 @@
+package com.laioffer.job.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Set;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Item {
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("location")
+    private String location;
+
+    @JsonProperty("company_logo")
+    private String companyLogo;
+
+    @JsonProperty("url")
+    private String url;
+
+    @JsonProperty("description")
+    private String description;
+
+    private Set<String> keywords;
+
+    private boolean favourite;
+
+    public void setKeywords(Set<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    public void setFavourite(boolean favourite) {
+        this.favourite = favourite;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCompanyLogo() {
+        return companyLogo;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Set<String> getKeywords() {
+        return keywords;
+    }
+
+    public boolean isFavourite() {
+        return favourite;
+    }
+}
---
+++ src/main/java/com/laioffer/job/external/GitHubClient.java
@@ -0,0 +1,64 @@
+package com.laioffer.job.external;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.Item;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import javax.swing.text.html.parser.Entity;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class GitHubClient {
+    private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
+
+    private static final String DEFAULT_KEYWORD = "developer";
+
+    public List<Item> search(double lat, double lon, String keyword){
+        if (keyword == null){
+            keyword = DEFAULT_KEYWORD;
+        }
+
+        try {
+            keyword = URLEncoder.encode(keyword,"UTF-8");
+        } catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+
+        String url = String.format(URL_TEMPLATE,keyword,lat,lon);
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        ResponseHandler<List<Item>> responseHandler = response -> {
+            if (response.getStatusLine().getStatusCode()!=200){
+                return Collections.emptyList();
+            }
+            HttpEntity entity = response.getEntity();
+
+            if (entity == null ){
+                return Collections.emptyList();
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            return Arrays.asList(mapper.readValue(entity.getContent(),Item[].class));
+
+            //return EntityUtils.toString(entity);//这里的entity是值得body吗？
+        };
+
+        try{
+            return httpClient.execute(new HttpGet(url),responseHandler);
+        } catch (IOException e){//会在网关出问题，network communication出问题的时候出现
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+}
